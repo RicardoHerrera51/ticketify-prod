9d753b5cdc9bc4b49b1c59860e777991ad264b07
María José Campos <48398181+majocampos92@users.noreply.github.com>
2023-07-05 01:15:26 -0600

    Update CartOrder.jsx

--- ticketify/src/pages/CartOrder/CartOrder.jsx
+++ ticketify/src/pages/CartOrder/CartOrder.jsx
@@ -2,8 +2,6 @@ import React from 'react'
 import Image1 from '../../assets/img/imagen2.png'
 import Footer from "../../components/Footer/Footer"
 import Navbar from "../../components/Navbar/Navbar"
-import iconCalendar from '../../assets/img/iconCalendar.png'
-import iconClock from '../../assets/img/iconClock.png'
 import iconCart from '../../assets/img/iconCart.png'
 import arrowBack from '../../assets/img/arrowBack.png'
 import { createTicketOrder } from '../../services/UserService'
@@ -19,50 +17,34 @@ function CartOrder() {
             async function fetchOrder() {
                 let response = await createTicketOrder(quantity, tier);
                 if (response.success) {
-                    setOrder(response);
+                    setOrder(response.result);
                 }
-                console.log(response)
             }
-        fetchOrder();
+            fetchOrder();
         }
-    }), [setOrder];
+    }), [setOrder, order];
 
     return (
         <div className="font-montserrat">
             <Navbar />
             <a href={'/preorder'}>
-            <div>
-                <img src={Image1} alt="Imagen" className="object-fill h-60 w-full" />
-            </div>
+                <div>
+                    <img src={Image1} alt="Imagen" className="object-fill h-60 w-full" />
+                </div>
 
-            <div className="flex items-center m-4">
-                <img src={arrowBack} alt="Google" className="h-4 w-2 inline-block" />
-                <p className="text-pure-indigo text-l font-extralight m-2">Atras</p>
-            </div>
+                <div className="flex items-center m-4">
+                    <img src={arrowBack} alt="Google" className="h-4 w-2 inline-block" />
+                    <p className="text-pure-indigo text-l font-extralight m-2">Atras</p>
+                </div>
             </a>
 
-            <p className="text-pure-indigo text-3xl font-extrabold m-4">{order?.result?.eventName}</p>
-
-            <div className="flex flex-wrap m-2">
-                <div className="p-2">
-                    <div className="bg-pure-indigo rounded-full py-2 px-4 flex items-center">
-                        <img src={iconCalendar} alt="Google" className="h-4 w-4 inline-block" />
-                        <p className="text-white font-normal inline-block ml-2">ABR 15, 2022</p>
-                    </div>
-                </div>
-                <div className="p-2">
-                    <div className="bg-pure-indigo rounded-full py-2 px-4 flex items-center">
-                        <img src={iconClock} alt="Google" className="h-4 w-4 inline-block" />
-                        <p className="text-white font-normal inline-block ml-2">7:00 PM</p>
-                    </div>
-                </div>
-            </div>
+            <p className="text-pure-indigo text-3xl font-extrabold m-4">{order?.eventName}</p>
 
             <div className="flex justify-center">
                 <div className="border shadow-md p-4 rounded-lg">
                     <h2 className="text-2xl text-center font-normal mb-4">Orden de pago</h2>
 
-                    <p className="font-normal text-center m-2">{order?.result?.eventName}</p>
+                    <p className="font-normal text-center m-2">{order?.eventName}</p>
 
                     <div className="grid grid-cols-2 gap-4">
                         <div>
@@ -71,8 +53,8 @@ function CartOrder() {
                         </div>
 
                         <div>
-                            <p className="font-normal py-2 px-4">{order?.result?.tier}</p>
-                            <p className="font-normal py-2 px-4">{order?.result?.totalTickets}</p>
+                            <p className="font-normal py-2 px-4">{order?.tier}</p>
+                            <p className="font-normal py-2 px-4">{order?.totalTickets}</p>
                         </div>
 
                     </div>
@@ -88,9 +70,9 @@ function CartOrder() {
                         </div>
 
                         <div>
-                            <p className="font-normal py-2 px-4">${order?.result?.subtotal}</p>
-                            <p className="font-normal py-2 px-4">${order?.result?.taxes}</p>
-                            <p className="font-normal text-lg py-2 px-4">${order?.result?.total}</p>
+                            <p className="font-normal py-2 px-4">${order?.subtotal}</p>
+                            <p className="font-normal py-2 px-4">${order?.taxes}</p>
+                            <p className="font-normal text-lg py-2 px-4">${order?.total}</p>
                         </div>
                     </div>
                 </div>
@@ -109,6 +91,5 @@ function CartOrder() {
         </div>
     )
 }
-
 
 export default CartOrder
