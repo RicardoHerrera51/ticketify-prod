5ffcc14832f4cb762280b7410f6a0446624eb92a
María José Campos <48398181+majocampos92@users.noreply.github.com>
2023-07-04 23:55:07 -0600

    Update ScannerView.jsx

--- ticketify/src/pages/ScannerView/ScannerView.jsx
+++ ticketify/src/pages/ScannerView/ScannerView.jsx
@@ -35,7 +35,7 @@ function Scanner() {
       }
       fetchTicketQr();
     }
-  }), [setTicket];
+  }), [setTicket, result];
 
   const handleDecode = (decodedResult) => {
     setResult(decodedResult)
